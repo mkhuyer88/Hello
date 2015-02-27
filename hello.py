3b2e1d564e2d31681c69193d0166461f8958a5d6
Mohammed Khuyer <mkhuyer88@gmail.com>
2015-02-27 12:26:39 +0000

    second change
    added comment

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@ import sys
 import os
 
 
-fruit = 'apples', 'pears', 'bananas', 'pears', 'apples', 'oranges', 'grapes'
+fruit = 'apples', 'pears', 'bananas', 'pears', 'apples', 'oranges', 'grapes', #list of fruits
 
 print (list(set(fruit)))
